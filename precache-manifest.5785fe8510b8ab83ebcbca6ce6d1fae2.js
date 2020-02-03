427fd6945f60fd1cf39bce65c31357683dd73d9e
mariiamoroziuk <moroziuk_m@ukr.net>
2020-02-03 10:07:30 +0200

    Updates

---
+++ precache-manifest.5785fe8510b8ab83ebcbca6ce6d1fae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c7678fa2a2d986e2a1dfb76405800c",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "05181b2cf65e2672d227",
+    "url": "/todoapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f85da613d8f0fa4eb747",
+    "url": "/todoapp/static/js/2.7ac6912d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/todoapp/static/js/2.7ac6912d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05181b2cf65e2672d227",
+    "url": "/todoapp/static/js/main.88c44d4d.chunk.js"
+  },
+  {
+    "revision": "ccbeb616d9e4a9b9b08a",
+    "url": "/todoapp/static/js/runtime-main.d2220a92.js"
+  }
+]);
